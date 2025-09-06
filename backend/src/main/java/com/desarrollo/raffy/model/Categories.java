92c4b2d60ebd36302cadf181b45174803563b3c7
GadielDhorta068 <gadel068@outlook.com>
2025-09-06 19:54:52 -0300

    Eventos entidad

--- backend/src/main/java/com/desarrollo/raffy/model/Categories.java
+++ backend/src/main/java/com/desarrollo/raffy/model/Categories.java
@@ -1,18 +1,30 @@
 package com.desarrollo.raffy.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 
 @Setter
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
+@Table(name = "categories")
 public class Categories {
     
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
 
+    @NotBlank(message = "El nombre de la categoría no puede estar vacío")
+    @Size(min = 2, max = 50, message = "El nombre debe tener entre 2 y 50 caracteres")
+    @Column(name = "name", nullable = false, unique = true, length = 50)
     private String name;
     
 }
