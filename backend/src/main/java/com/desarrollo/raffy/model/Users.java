64a956591c6ab64a330b794a608a20d22a9a89f7
javiercalandri96 <javiercalandri96@gmail.com>
2025-09-07 15:34:34 -0300

    cambia nombre abstractUsers por Users

---
+++ backend/src/main/java/com/desarrollo/raffy/model/Users.java
@@ -0,0 +1,45 @@
+package com.desarrollo.raffy.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@Table(name = "Users")
+
+@Getter
+@Setter
+
+public abstract class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_id_seq")
+    @SequenceGenerator(name = "user_id_seq", sequenceName = "user_idseq", initialValue = 0, allocationSize = 1)
+    private Long id;
+
+    private String name;
+
+    private String surname;
+
+    @Column(unique = true)
+    @NotBlank(message = "El campo email es obligatorio") // pasar a ingles jaja
+    @Email(message = "El Email debe tener un formato valido") // pasar a ingles jaja x2
+    private String email;
+
+    @Column(unique = true)
+    private String cellphone;
+    
+}
