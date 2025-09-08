02fd1c8a31eaf63b180097a33ff2563ae26ab4c6
GadielDhorta068 <gadel068@outlook.com>
2025-09-07 21:04:56 -0300

    Preparando front de usuario

---
+++ frontend/src/app/pages/user-config/user-config.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-config',
+  imports: [],
+  templateUrl: './user-config.html',
+  styleUrl: './user-config.css'
+})
+export class UserConfig {
+
+}
---
+++ frontend/src/app/shared/components/user-login-card/user-login-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-login-card',
+  imports: [],
+  templateUrl: './user-login-card.html',
+  styleUrl: './user-login-card.css'
+})
+export class UserLoginCard {
+
+}
