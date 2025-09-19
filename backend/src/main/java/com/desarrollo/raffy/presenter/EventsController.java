fed9f89cf4b316da11951995baf762a6f16b15b6
javiercalandri96 <javiercalandri96@gmail.com>
2025-09-19 18:26:27 -0300

    agrega metodo para inscribir guestUsers a sorteo

--- backend/src/main/java/com/desarrollo/raffy/presenter/EventsController.java
+++ backend/src/main/java/com/desarrollo/raffy/presenter/EventsController.java
@@ -13,11 +13,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import com.desarrollo.raffy.model.Events;
 import com.desarrollo.raffy.model.Giveaways;
+import com.desarrollo.raffy.model.GuestUser;
 import com.desarrollo.raffy.model.RegisteredUser;
 import com.desarrollo.raffy.model.StatusEvent;
+import com.desarrollo.raffy.model.User;
 import com.desarrollo.raffy.model.EventTypes;
 import com.desarrollo.raffy.business.services.EventsService;
 import com.desarrollo.raffy.business.services.GiveawaysService;
+import com.desarrollo.raffy.business.services.ParticipantService;
+import com.desarrollo.raffy.business.services.UserService;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
@@ -36,6 +40,13 @@ public class EventsController {
 
     @Autowired
     private GiveawaysService giveawaysService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ParticipantService participantService;
+
 
     @PostMapping
     public ResponseEntity<?> create(@Valid @RequestBody Events events) {
@@ -278,5 +289,50 @@ public class EventsController {
         return new ResponseEntity<>(giveaways, HttpStatus.OK);
     }
 
+    @PostMapping("/{eventId}/participants")
+    public ResponseEntity<Object> registerParticipantToGiveaway(
+        @Valid @RequestBody GuestUser aGuestUser,
+        @PathVariable("eventId") Long aEventId) {
+            if (aGuestUser.getId() != 0) {
+                return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("Esta intentando crear un guest user. Este no puede tener un id definido.");
+            }
+
+            Events eventToParticipate = eventsService.getById(aEventId);
+            if (eventToParticipate == null) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("El evento con id " + aEventId + " no existe");
+            }
+
+            User savedGuestUser;
+            User userFromDb = userService.findByEmail(aGuestUser.getEmail());
+            
+            // chequea si ya existe el usuario en la base de datos
+            if (userFromDb == null) {
+                // si el usuario no existe, lo guarda
+                savedGuestUser = userService.save(aGuestUser);
+            }
+            else {
+                // si el usuario existe lo actualizo
+                userFromDb.setName(aGuestUser.getName());
+                userFromDb.setSurname(aGuestUser.getSurname());
+                userFromDb.setCellphone(aGuestUser.getCellphone());
+                
+                savedGuestUser = userService.save(userFromDb);
+            }
+            try {
+                // intento guardar la participacion del usuario
+                
+                return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(participantService.registerToGiveaway(savedGuestUser, eventToParticipate));    
+            } catch (Exception e) {
+                return ResponseEntity
+                    .status(HttpStatus.CONFLICT)
+                    .body(e.getMessage());
+            }
+            
+    }
 
 }
