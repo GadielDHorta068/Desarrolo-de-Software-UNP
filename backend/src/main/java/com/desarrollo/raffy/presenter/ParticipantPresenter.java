cc9cf3d7a39a58bdff4bdca011dbd75df9cdfcf1
javiercalandri96 <javiercalandri96@gmail.com>
2025-09-19 18:17:00 -0300

    minnor changes

--- backend/src/main/java/com/desarrollo/raffy/presenter/ParticipantPresenter.java
+++ backend/src/main/java/com/desarrollo/raffy/presenter/ParticipantPresenter.java
@@ -2,18 +2,16 @@ package com.desarrollo.raffy.presenter;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+
+import jakarta.validation.Valid;
 
 import com.desarrollo.raffy.business.services.EventsService;
 import com.desarrollo.raffy.business.services.ParticipantService;
@@ -21,8 +19,6 @@ import com.desarrollo.raffy.business.services.UserService;
 import com.desarrollo.raffy.model.Events;
 import com.desarrollo.raffy.model.GuestUser;
 import com.desarrollo.raffy.model.User;
-
-import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/events")
