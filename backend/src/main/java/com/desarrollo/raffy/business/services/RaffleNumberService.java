184a0048faeae3efb88ce8a848327eba91284b5f
javiercalandri96 <javiercalandri96@gmail.com>
2025-10-08 18:37:48 -0300

    usa agrega checkeo de la ecistencia de un numero

--- backend/src/main/java/com/desarrollo/raffy/business/services/RaffleNumberService.java
+++ backend/src/main/java/com/desarrollo/raffy/business/services/RaffleNumberService.java
@@ -3,6 +3,7 @@ package com.desarrollo.raffy.business.services;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,7 +27,7 @@ public class RaffleNumberService {
 
     public List<RaffleNumber> findRaffleNumbersById(Raffle aRaffle) {
         if (!(aRaffle instanceof Raffle)) {
-            throw new IllegalArgumentException("Este metodo es solo para asignar numeros de rifa a usuarios")
+            throw new IllegalArgumentException("Este metodo es solo para asignar numeros de rifa a usuarios");
         }
         List<RaffleNumber> result = raffleNumRepository.findNumbersById(aRaffle.getId());
 
@@ -37,9 +38,16 @@ public class RaffleNumberService {
     public List<RaffleNumber> createRaffleNumbers(Events aRaffle, User aUser, List<Integer> someNumbers) {
         List<RaffleNumber> result = new ArrayList<>();
         for (int i = 0; i < someNumbers.size(); i++) {
-            RaffleNumber newNumber = new RaffleNumber(aRaffle, aUser, someNumbers.get(i));
-            raffleNumRepository.save(newNumber);
-            result.add(newNumber);
+            int numberToBuy = someNumbers.get(i);
+            if (!raffleNumRepository.existsByRaffleAndNumber((Raffle)aRaffle, numberToBuy)) {
+                RaffleNumber newNumber = new RaffleNumber(aRaffle, aUser, numberToBuy);
+                raffleNumRepository.save(newNumber);
+                result.add(newNumber);
+            }
+            else {
+                // Remplazar por una excepcion mejor
+                throw new IllegalArgumentException("Estas intentando comprar un numero que ya tiene dueño");
+            }
         }
         return result;
     }
