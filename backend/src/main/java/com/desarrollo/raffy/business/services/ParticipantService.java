063ec487373da13f551fe2a121e7128d501b46cc
BrandonAmarillo <brandonxdamarillo@gmail.com>
2025-10-25 20:48:08 -0300

    comiteado

--- backend/src/main/java/com/desarrollo/raffy/business/services/ParticipantService.java
+++ backend/src/main/java/com/desarrollo/raffy/business/services/ParticipantService.java
@@ -34,6 +34,39 @@ public class ParticipantService {
     @Autowired
     private WinnerStrategyFactory strategyFactory;
 
+    /* public List<Participant> runEvent(Events event){
+        try {       
+            log.info("Evento encontrado: " + event.getTitle() + " con estado " + event.getStatusEvent());
+
+            List<Participant> participants = participantRepository.findParticipantsByEventId(event.getId());
+            log.info("Participantes: " + participants);
+            if(participants.isEmpty()){
+                throw new RuntimeException("No hay participantes en este sorteo");
+            }
+
+            // Obtener la estrategia adecuada
+            WinnerSelectionStrategy strategy = strategyFactory.getStrategy(event.getEventType());
+            log.info("Estrategia seleccionada: " + (strategy != null ? strategy.getClass().getSimpleName() : "Ninguna"));
+            if (strategy == null) {
+                throw new UnsupportedOperationException("No hay estrategia definida para este tipo de evento: " + event.getEventType());
+            }
+
+            strategy.selectWinners(event, participants);
+            log.info("Participantes después de seleccionar ganadores: " + participants);
+            // Persistir cambios (posiciones actualizadas)
+            participantRepository.saveAll(participants);
+
+            //Devolver solo los ganadores
+            return participants.stream()
+            .filter(p -> p.getPosition() > 0)
+            .toList();
+        } catch (Exception e) {
+            log.error("Error al ejecutar el evento: " + e.getMessage(), e);
+            throw e; // Re-lanzar la excepción para que pueda ser manejada por el controlador
+        }
+        
+        } */
+
     @Transactional
     public List<?> runEvents(Events event) {
         try {
