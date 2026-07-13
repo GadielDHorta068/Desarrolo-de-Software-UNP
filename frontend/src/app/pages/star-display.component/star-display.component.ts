3d1bcbb7df51da823140c057472b55c1fefa4ce7
DobleTmatutin0 <zz_00024@hotmail.com>
2026-07-13 19:35:40 -0300

    nuevo componente para mostarar las estrellas en el perfil de usuario

---
+++ frontend/src/app/pages/star-display.component/star-display.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-star-display.component',
+  imports: [],
+  templateUrl: './star-display.component.html',
+  styleUrl: './star-display.component.css'
+})
+export class StarDisplayComponent {
+
+}
