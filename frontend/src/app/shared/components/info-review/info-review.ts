9117d7a190ae43a1aba4e0188971789fd8b85800
Brandon Amarillo <brandonxdamarillo@gmail.com>
2026-09-25 15:40:07 -0300

    feat(component): corrección en la el componente de la info de las reseñas.

--- frontend/src/app/shared/components/info-review/info-review.ts
+++ frontend/src/app/shared/components/info-review/info-review.ts
@@ -2,13 +2,13 @@ import { Component, Input, OnInit } from '@angular/core';
 import { HandleReportStatusPipe } from '../../../pipes/handle-report-status.pipe';
 import { CommonModule } from '@angular/common';
 import { reviewFromBacktoFrontDTO } from '../../../models/review/reviewFromBacktoFrontDTO';
-import { StarRatingComponent } from '../../../pages/star-rating.component/star-rating.component';
 import { Router } from '@angular/router';
 import { HandleDeliveryPipe } from '../../../pipes/handle-delivery.pipe';
+import { StarDisplayComponent } from '../../../pages/star-display.component/star-display.component';
 
 @Component({
   selector: 'app-info-review',
-  imports: [CommonModule, StarRatingComponent, HandleDeliveryPipe],
+  imports: [CommonModule, StarDisplayComponent, HandleDeliveryPipe],
   templateUrl: './info-review.html',
   styleUrl: './info-review.css'
 })
