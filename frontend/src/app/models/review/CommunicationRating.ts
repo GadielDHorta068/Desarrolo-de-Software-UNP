87ebe6211cd4b0cd59a56315f432c4ce02bc9122
Brandon Amarillo <brandonxdamarillo@gmail.com>
2026-09-25 12:45:14 -0300

    feat(models): se agregaron atributos en los dto de las reseñas y los enum AwardAliagnment y CommunicationRating.

---
+++ frontend/src/app/models/review/CommunicationRating.ts
@@ -0,0 +1,6 @@
+export enum CommunicationRating {
+    EXCELENTE = 'EXCELENTE',
+    BUENA = 'BUENA',
+    REGULAR = 'REGULAR',
+    MALA = 'MALA'
+}
--- frontend/src/app/models/review/reviewFromBacktoFrontDTO.ts
+++ frontend/src/app/models/review/reviewFromBacktoFrontDTO.ts
@@ -1,3 +1,5 @@
+import { AwardAlignment } from "./AwardAlignment";
+import { CommunicationRating } from "./CommunicationRating";
 import { DeliveryStatus } from "./DeliveryStatus";
 
 export interface reviewFromBacktoFrontDTO {
@@ -7,6 +9,8 @@ export interface reviewFromBacktoFrontDTO {
     eventTitle: string,
     eventId: string,
     delivery: DeliveryStatus,
+    awardAlingment: AwardAlignment,
+    communicationRating: CommunicationRating,
     score: number,
     comment: string
 }
