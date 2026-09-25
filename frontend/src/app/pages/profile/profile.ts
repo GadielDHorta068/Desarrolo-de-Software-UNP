931b7bb67334290ce2e435082116ec7c46f8c36b
Brandon Amarillo <brandonxdamarillo@gmail.com>
2026-09-25 15:16:51 -0300

    feat(component): importar componente StarDisplayComponent en el perfil.

--- frontend/src/app/pages/profile/profile.ts
+++ frontend/src/app/pages/profile/profile.ts
@@ -13,13 +13,13 @@ import { HandleStatusPipe } from '../../pipes/handle-status.pipe';
 import { Meta, Title } from '@angular/platform-browser';
 import { Reviews } from '../reviews/reviews';
 import { ReviewService } from '../../services/review.service';
-import { StarRatingComponent } from '../star-rating.component/star-rating.component';
-import { HandleTypePipe } from '../../pipes/handle-type.pipe';
+import { StarDisplayComponent } from '../star-display.component/star-display.component';
+
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule, HttpClientModule, ClipboardModule, DrawCard, RouterModule, HandleStatusPipe, Reviews, StarRatingComponent, HandleTypePipe],
+  imports: [CommonModule, HttpClientModule, ClipboardModule, DrawCard, RouterModule, HandleStatusPipe, Reviews, StarDisplayComponent],
   templateUrl: './profile.html',
   styleUrl: './profile.css',
   animations: [
