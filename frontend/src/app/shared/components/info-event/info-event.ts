c2a824f193a42debbd2d1823dc84986bcc2f4924
Brandon Amarillo <brandonxdamarillo@gmail.com>
2026-09-25 15:42:10 -0300

    feat(component): se importa el componente de star-display en info-event.

--- frontend/src/app/shared/components/info-event/info-event.ts
+++ frontend/src/app/shared/components/info-event/info-event.ts
@@ -8,13 +8,13 @@ import { TagCategory } from '../tag-category/tag-category';
 import { HandleStatusPipe } from '../../../pipes/handle-status.pipe';
 import { WinnerDTO } from '../../../models/winner.model';
 import { TagPrize } from '../tag-prize/tag-prize';
-import { StarRatingComponent } from '../../../pages/star-rating.component/star-rating.component';
+import { StarDisplayComponent } from '../../../pages/star-display.component/star-display.component';
 import { ReviewService } from '../../../services/review.service';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-info-event',
-  imports: [CommonModule, HandleDatePipe, HandleTypePipe, HandleIconTypePipe, HandleStatusPipe, TagCategory, TagPrize, StarRatingComponent],
+  imports: [CommonModule, HandleDatePipe, HandleTypePipe, HandleIconTypePipe, HandleStatusPipe, TagCategory, TagPrize, StarDisplayComponent],
   templateUrl: './info-event.html',
   styleUrl: './info-event.css'
 })
